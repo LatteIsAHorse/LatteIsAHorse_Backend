6fa0eb2f515eae5ab24147eedc619c1fb5de289d
Minsu (Alex) Ji <alexji.dev@gmail.com>
2021-11-01 20:36:41 +0900

    [#2] chore : 프로젝트 초기 셋팅 (#3)
    - Spring Boot 초기 설정
    - .gitignore 셋팅

---
+++ src/main/java/team/latte/LatteIsAHorse/LatteIsAHorseApplication.java
@@ -0,0 +1,13 @@
+package team.latte.LatteIsAHorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteIsAHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatteIsAHorseApplication.class, args);
+	}
+
+}
