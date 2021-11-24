b7a10cb0ab6db14aff9b41a506cf76d508adc60d
Minsu (Alex) Ji <alexji.dev@gmail.com>
2021-11-25 00:11:09 +0900

    [#40] chore : 라떼 포인트 내역 API (#42)
    - LatteStackInfoRepository를 querydsl로 변경
    - 조회 조건으로 LatteStackInfoSearch 추가

--- src/main/java/team/latte/LatteIsAHorse/service/LatteStackInfoService.java
+++ src/main/java/team/latte/LatteIsAHorse/service/LatteStackInfoService.java
@@ -1,8 +1,11 @@
 package team.latte.LatteIsAHorse.service;
 
+import team.latte.LatteIsAHorse.dto.AllLatteStackInfoRes;
+import team.latte.LatteIsAHorse.dto.LatteStackInfoSearch;
+
 import java.util.List;
 
 public interface LatteStackInfoService {
 
-    List<?> allLatteStackInfoList(String userEmail);
+    List<AllLatteStackInfoRes> allLatteStackInfoList(LatteStackInfoSearch latteStackInfoSearch, String userEmail);
 }
