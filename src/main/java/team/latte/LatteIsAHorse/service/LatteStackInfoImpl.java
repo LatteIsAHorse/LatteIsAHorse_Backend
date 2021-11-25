e24dfe8d1b5bad2ff9a1356468a97d29554c8a19
alexjime <alexji.dev@gmail.com>
2021-11-25 10:43:00 +0900

    [#43] feat : 사용 가능한 라떼 포인트 조회 API feat : 사용 가능한 라떼 포인트 조회 API - LatteStackInfoController, LatteStackInfoService에 관련 로직 추가 style : LatteStackInfoController, LatteStackInfoService - API URI 변경

--- src/main/java/team/latte/LatteIsAHorse/service/LatteStackInfoImpl.java
+++ src/main/java/team/latte/LatteIsAHorse/service/LatteStackInfoImpl.java
@@ -4,10 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.latte.LatteIsAHorse.config.exception.CustomException;
+import team.latte.LatteIsAHorse.config.response.ExceptionStatus;
 import team.latte.LatteIsAHorse.dto.AllLatteStackInfoRes;
 import team.latte.LatteIsAHorse.dto.LatteStackInfoSearch;
 import team.latte.LatteIsAHorse.model.user.LatteStackInfo;
+import team.latte.LatteIsAHorse.model.user.RoleType;
+import team.latte.LatteIsAHorse.model.user.User;
 import team.latte.LatteIsAHorse.repository.LatteStackInfoRepository;
+import team.latte.LatteIsAHorse.repository.UserRepository;
+
 import java.util.List;
 
 @Service
@@ -17,6 +23,21 @@ import java.util.List;
 public class LatteStackInfoImpl implements LatteStackInfoService{
 
     private final LatteStackInfoRepository latteStackInfoRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 사용 가능한 라떼 포인트 조회
+     * @param userEmail : 유저 이메일
+     * @return
+     */
+    @Override
+    public int getAvailableLattepoint(String userEmail) {
+        User user = userRepository.findByEmailAndRoleOnlyNotLeave(userEmail, RoleType.ROLE_UNKNOWN).orElse(null);
+
+        if (user == null) throw new CustomException(ExceptionStatus.BAD_REQUEST_ERROR);
+
+        return user.getLatteStack();
+    }
 
     /**
      * 라떼 포인트 내역 조회
@@ -32,4 +53,5 @@ public class LatteStackInfoImpl implements LatteStackInfoService{
 
         return AllLatteStackInfoRes.listOf(latteStackInfos);
     }
+
 }
